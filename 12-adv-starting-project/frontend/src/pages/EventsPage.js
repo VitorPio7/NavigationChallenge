201281d9540218e73000a181d1e8658ee0c51e03
VitorPio7 <vitorvpio60@gmail.com>
2025-02-21 18:10:40 -0300

    today
    today

--- 12-adv-starting-project/frontend/src/pages/EventsPage.js
+++ 12-adv-starting-project/frontend/src/pages/EventsPage.js
@@ -1,23 +1,31 @@
-import { useLoaderData } from 'react-router-dom';
-
+import { useLoaderData, Await } from 'react-router-dom';
+import { Suspense } from 'react';
 import EventsList from '../components/EventsList';
 
 function EventsPage() {
     const data = useLoaderData();
     const events = data.events;
-
-    return <EventsList events={events} />;
+    return <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
+        <Await resolve={events}>
+            {(loadedEvents) => <EventsList events={loadedEvents} />}
+        </Await>
+    </Suspense >
 }
 
 export default EventsPage;
-
-export async function loader() {
+export async function loadEvents() {
     const response = await fetch('http://localhost:8080/events');
     if (!response.ok) {
         throw new Response(JSON.stringify({ message: 'Could not fetch events.' }), {
             status: 500,
         });
     } else {
-        return response;
+        let resData = await response.json();
+        return resData.events;
     }
+}
+export async function loader() {
+    return {
+        events: loadEvents(),
+    };
 }
